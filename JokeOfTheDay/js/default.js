3616d2f2ba37395991f7534969c2c1e1665003e8
Miguel Santirso <miguel@pncil.com>
2012-11-04 12:53:56 +0100

    Initial version of the app
    Basic functionality working

---
+++ JokeOfTheDay/js/default.js
@@ -0,0 +1,83 @@
+﻿// For an introduction to the Fixed Layout template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232508
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                registerForShare();
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+
+            setDate();
+            setJokeOfDay();
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.onloaded = function () {
+        WinJS.Resources.processAll();
+    }
+
+
+    function setDate() {
+        var dateFormatter = new Windows.Globalization.DateTimeFormatting.DateTimeFormatter("longdate");
+        var formattedDate = dateFormatter.format(new Date());
+
+        var theDate = document.getElementById("date");
+        theDate.textContent = formattedDate;
+    }
+
+    function setJokeOfDay() {
+        var theJoke = document.getElementById("theJoke");
+        theJoke.textContent = getTodaysJoke();
+    }
+
+
+    function getTodaysJoke() {
+        var nJokes = getString("/jokes/nJokes");
+        var today = Math.floor(new Date().getTime() / (24 * 60 * 60 * 1000));
+
+        var todaysJokeI18NKey = "/jokes/joke" + today % nJokes;
+
+        return getString(todaysJokeI18NKey);
+    }
+
+
+    function registerForShare() {
+        var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+        dataTransferManager.addEventListener("datarequested", shareTextHandler);
+    }
+
+    function shareTextHandler(e) {
+        var request = e.request;
+        request.data.properties.title = getString("sharePaneTitle");
+        request.data.properties.description = getString("sharePaneDescription");
+        request.data.setText(getTodaysJoke());
+    }
+
+
+    function getString(i18nKey) {
+        return WinJS.Resources.getString(i18nKey).value;
+    }
+
+
+    app.start();
+})();
