f756c7c5d6cdda309c435d34de32236cb93969fc
Miguel Santirso <miguel@pncil.com>
2012-11-05 21:30:25 +0100

    Added "about" page in settings pane

--- JokeOfTheDay/js/default.js
+++ JokeOfTheDay/js/default.js
@@ -28,6 +28,11 @@
         refreshUI();
     };
 
+    app.onsettings = function (e) {
+        e.detail.applicationcommands = { "about": { title: "About", href: "/html/about.html" } };
+        WinJS.UI.SettingsFlyout.populateSettings(e);
+    };
+
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
         // that needs to persist across suspensions here. You might use the
